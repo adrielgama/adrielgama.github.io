ade7399174a2839e59392ba5f244cc6927474df9
Adriel Gama <adrielgama@gmail.com>
2023-11-24 09:22:58 -0300

    feat: create darktheme provider and toggle

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,31 @@
+import { Moon, Sun } from 'lucide-react'
+
+import { useTheme } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light')
+    } else {
+      setTheme('dark')
+    }
+  }
+
+  return (
+    <>
+      <Button variant="outline" size="icon">
+        <Sun
+          className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          onClick={toggleTheme}
+        />
+        <Moon
+          className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          onClick={toggleTheme}
+        />
+      </Button>
+    </>
+  )
+}
