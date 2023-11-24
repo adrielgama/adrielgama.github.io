388d335c16bdc92157bd909381295afe59d374d2
Adriel Gama <adrielgama@gmail.com>
2023-11-24 18:42:43 -0300

    fix: toogle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { ModeToggle } from '@/components/mode-toggle'
+import { Presentation } from '@/components/presentation'
+import { Skills } from '@/components/skills'
 
 function App() {
   return (
-    <div className="h-screen w-screen dark:bg-black bg-slate-50">
-      <ModeToggle />
+    <div className="h-screen w-screen dark:bg-background bg-slate-50">
+      <div className="container flex justify-end pt-2">
+        <ModeToggle />
+      </div>
+      <div className="container">
+        <Presentation />
+        <Skills />
+      </div>
     </div>
   )
 }
