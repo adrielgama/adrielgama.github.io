e32ee0a44a081810de38e80d382a508e09a57b4b
Adriel Gama <adrielgama@gmail.com>
2023-11-26 00:20:08 -0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://adrielgama.dev',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
