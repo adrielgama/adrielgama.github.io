5363e029e97fb120fbef8ecec472452bc07a839d
Adriel Gama <adrielgama@gmail.com>
2023-11-26 00:33:20 -0300

    fix: gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://adrielgama.dev',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
