a5382a228fd54cf0491266fc151c6f90b0915bb0
Adriel Gama <adrielgama@gmail.com>
2024-01-16 23:49:43 -0300

    perf: automatic calc to years of experience

--- src/presentation/about/index.tsx
+++ src/presentation/about/index.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { SocialIcons } from '../../components/social'
 
 export const AboutMe = () => {
+  const yearsOfExperience = new Date().getFullYear() - 2019
+
   return (
     <div className="flex flex-col justify-center py-12">
       <p className="text-sm">Hi, I&rsquo;m Adriel</p>
@@ -16,11 +18,11 @@ export const AboutMe = () => {
         Developer
       </h1>
       <p className="mt-8 max-w-2xl text-justify text-sm md:max-w-6xl md:px-20 lg:px-32">
-        I&rsquo;m a Front-end developer with 5+ years of experience in
-        responsive web projects. My strength lies in translating complex designs
-        into interactive and efficient interfaces, ensuring compatibility with
-        various browsers and devices, with the aim of delivering innovative
-        solutions that enhance the user experience.
+        I&rsquo;m a Front-end developer with {yearsOfExperience}+ years of
+        experience in responsive web projects. My strength lies in translating
+        complex designs into interactive and efficient interfaces, ensuring
+        compatibility with various browsers and devices, with the aim of
+        delivering innovative solutions that enhance the user experience.
       </p>
     </div>
   )
